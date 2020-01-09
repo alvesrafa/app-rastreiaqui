754d854d381d22199254a495a828b2daf43df63b
Rafael Alves <alvesrafael17@hotmail.com>
2020-01-08 23:22:51 -0300

    loading svg

--- src/pages/Objeto/index.js
+++ src/pages/Objeto/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './master.css';
+import {ThreeDots  } from 'svg-loaders-react';
 
 class Objeto extends Component {
     constructor(props) {
@@ -7,28 +8,32 @@ class Objeto extends Component {
     
         this.state = {
           objeto: '',
-          status: false,
+          status: '',
           codigo: '',
+          carregando: true,
 
         };
 
     }
     componentDidMount() {
+      
       fetch(`https://rastreiaqui-back.herokuapp.com/rastrear/${this.props.match.params.codigoRastreio}`)
       .then(res => res.json())
           .then((result) => {
             this.setState({objeto: result[0]})
-
             console.log(this.state.objeto)
             if(this.analisarObjeto(this.state.objeto)){
               this.setState({status : true})
+              this.setState({carregando: false})
             }
         },
         (error) => {
           console.log('erro')
         }
       )
+      
   }
+
   analisarObjeto(objeto){
     if (objeto.isInvalid === true) return false;
 
@@ -46,17 +51,20 @@ class Objeto extends Component {
 }
     render() {
        return (
-        
-          <div className="">
-            <div className="text-center">Código: {this.state.objeto.code}</div>
+          <div className="text-center">
+            {this.state.carregando === true &&(
+              <ThreeDots  stroke="#000"/>
+            )}
             {this.state.status === false &&(
-              <div>Código invalido</div>
+              <div>
+                <div className="text-center">Código: {this.state.objeto.code}</div>
+                <div>Código invalido</div>
+              </div>
             )}
             {this.state.status === true &&(
-              
               this.state.objeto.tracks.map((track) => (
                 <div className="row objeto">
-                  <div className="col-12 text-center icon"><i class="fas fa-long-arrow-alt-down"></i></div>
+                  <div className="col-12 text-center icon"><i className="fas fa-long-arrow-alt-down"></i></div>
                   <div className="col-2 side">
                     <div>{this.dataFormatada(track.trackedAt)}</div>
                     <p>{track.locale}</p>
@@ -65,11 +73,9 @@ class Objeto extends Component {
                     <p>{track.status}</p>
                     {track.observation}
                   </div>
-                  
                 </div>
-            ))
+              ))
             )}
-            
           </div>
        )
         
