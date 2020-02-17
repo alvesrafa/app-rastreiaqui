a1ae83da6e7df1b0320f9693bb387acf98edacc3
Rafael Alves <alvesrafael17@hotmail.com>
2020-02-17 15:21:06 -0300

    contato ok

--- frontend/src/pages/Contato/index.js
+++ frontend/src/pages/Contato/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import api from '../../services/api';
 
 import './contato.css';
 
@@ -6,18 +7,17 @@ const Contato = () => {
     const [email, setEmail] = useState('');
     const [info, setInfo] = useState('');
 
-    function handleContato(e){
+    async function handleContato(e){
         e.preventDefault();
         
-
-        
+        await api.post('/email', {email, info})
         setEmail('');
         setInfo('');
         alert('Mensagem entregue com sucesso! Obrigado por me ajudar a evoluir ")')
     }
     return (
         <div id="contato">
-            <h1>Contato</h1>
+            <h2>Contato</h2>
             <form onSubmit={handleContato}>
                 <input type="email" onChange={e => setEmail(e.target.value)} required placeholder="Seu email" value={email} />
                 <textarea required onChange={e => setInfo(e.target.value)} cols="30" rows="10" placeholder="Mensagens construtivas (:" value={info} />
