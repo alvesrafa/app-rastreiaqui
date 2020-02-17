7247b8684c0da79f6d2406f82bf5e44216cdfeb6
Rafael Alves <alvesrafael17@hotmail.com>
2020-02-17 15:31:56 -0300

    email

---
+++ src/app/controller/emailController.js
@@ -0,0 +1,27 @@
+const mailer = require('nodemailer');
+import {EMAIL_USERNAME, EMAIL_PASS} from '../../../env.json';
+const config = {
+    host: 'smtp.mailtrap.io',
+    port: 25,
+    auth: {
+        user: '3d9aedc0e2bc3e',
+        pass: '674a649a80ef76'
+    }
+}
+
+const transporter = mailer.createTransport(config)
+
+exports.enviarEmail = (req,res) => {
+  
+  const message = {
+    from: req.body.email,
+    to: 'alvesrafael17@hotmail.com',
+    subject: 'Contato',
+    text: req.body.info
+  }
+  transporter.sendMail(message, (error, info) => {
+    if (error) return res.status(400).json({sended: false})
+
+    return res.status(200).json({sended: true})
+  })
+} 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
-const rastro = require('./Rastro.js')
 
+const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 
 const correioController = require('./app/controller/correioController');
+const emailController = require('./app/controller/emailController');
 
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin','*'); //dominio do 'front'
-    res.setHeader('Access-Control-Allow-Methods','*');//GET, POST, PUT, DELETE, PATCH, OPTIONS
-    res.setHeader('Access-Control-Allow-Headers','*'); // ou Content-Type
-    next();
-});
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json())
 
-app.get('/rastrear/:codigo', correioController.veriicarCodigo)
+app.get('/rastrear/:codigo', correioController.veriicarCodigo);
+
+app.post('/email', emailController.enviarEmail);
 
 server.listen(process.env.PORT || 3001, () => {
-    console.log('opaaaa')
+    console.log('Servidor rodando')
 });
