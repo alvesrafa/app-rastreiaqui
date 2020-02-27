124cdf4fd21efc82e6b16ed7a5e7c016fe87efdf
Rafael Alves <alvesrafael17@hotmail.com>
2020-02-27 12:55:23 -0300

    .

--- mobile/src/components/Objeto.js
+++ mobile/src/components/Objeto.js
@@ -114,8 +114,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex:1,
-    alignItems: 'center',
-    justifyContent:'space-around',
+    alignItems: 'center'
   },
   objetos: {
     flex:1,
